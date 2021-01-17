2f07f0d3818b35d978c319c92aca8d1a04081ec5
Yasha Jannoo <alledodo@gmail.com>
2021-01-17 22:51:16 +0000

    Commit minimal info required to create project
    Engine version 2018.2.15f1

---
+++ Assets/Scripts/AndroidBridge.cs
@@ -0,0 +1,114 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AndroidBridge : MonoBehaviour
+{
+    [SerializeField] private Button startListeningBtn = null;
+    [SerializeField] private Toggle continuousListeningTgle = null;
+    [SerializeField] private Text resultsTxt = null;
+
+    public static string ANDROIDBRIDGE_GO_NAME = "AndroidBridge";
+
+    public interface IAndroidBridge
+    {
+        void OnResult( string recognizedResult );
+    }
+
+    public static T AndroidCall<T>( string method, params object[] args )
+    {
+        using ( AndroidJavaClass jc = new AndroidJavaClass( "com.unity3d.player.UnityPlayer" ) )
+        {
+            using ( AndroidJavaObject jo = jc.GetStatic<AndroidJavaObject>( "currentActivity" ) )
+            {
+                if ( args != null )
+                    return jo.Call<T>( method, args );
+                else
+                    return jo.Call<T>( method );
+            }
+        }
+    }
+
+    public static void AndroidCall( string method, params object[] args )
+    {
+        using ( AndroidJavaClass jc = new AndroidJavaClass( "com.unity3d.player.UnityPlayer" ) )
+        {
+            using ( AndroidJavaObject jo = jc.GetStatic<AndroidJavaObject>( "currentActivity" ) )
+            {
+                if ( args != null )
+                    jo.Call( method, args );
+                else
+                    jo.Call( method );
+            }
+        }
+    }
+
+    public static void AndroidStaticCall( string method, params object[] args )
+    {
+        using ( AndroidJavaClass jc = new AndroidJavaClass( "com.unity3d.player.UnityPlayer" ) )
+        {
+            using ( AndroidJavaObject jo = jc.GetStatic<AndroidJavaObject>( "currentActivity" ) )
+            {
+                if ( args != null )
+                    jo.CallStatic( method, args );
+                else
+                    jo.CallStatic( method );
+            }
+        }
+    }
+
+    public static void AndroidRunnableCall( string method, params object[] args )
+    {
+        using ( AndroidJavaClass jc = new AndroidJavaClass( "com.unity3d.player.UnityPlayer" ) )
+        {
+            using ( AndroidJavaObject jo = jc.GetStatic<AndroidJavaObject>( "currentActivity" ) )
+            {
+                if ( args != null )
+                {
+                    jo.Call( "runOnUiThread", new AndroidJavaRunnable( () =>
+                    {
+                        AndroidCall( method, args );
+                    } ) );
+                }
+                else
+                {
+                    jo.Call( "runOnUiThread", new AndroidJavaRunnable( () =>
+                    {
+                        AndroidCall( method );
+                    } ) );
+                }
+            }
+        }
+    }
+
+
+    void Start()
+    {
+        this.gameObject.name = ANDROIDBRIDGE_GO_NAME;
+
+        startListeningBtn.onClick.AddListener(StartListening);
+        continuousListeningTgle.onValueChanged.AddListener(SetContinuousListening);
+    }
+
+    private void StartListening()
+    {
+        AndroidRunnableCall("StartListening");
+    }
+
+    private void SetContinuousListening(bool isContinuous)
+    {
+        AndroidCall("SetContinuousListening", isContinuous);
+    }
+
+    public void OnResult(string recognizedResult)
+    {
+        char[] delimiterChars = { '~' };
+        string[] result = recognizedResult.Split(delimiterChars);
+
+        resultsTxt.text = "";
+        for (int i = 0; i < result.Length; i++)
+        {
+            resultsTxt.text += result[i] + '\n';
+        }
+    }
+
+}
